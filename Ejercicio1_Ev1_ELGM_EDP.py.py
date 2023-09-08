560f937ebd30906599505ebd714bd3964cc11ca1
EvelynGM14 <118089029+EvelynGM14@users.noreply.github.com>
2023-09-08 13:56:25 -0600

    Add files via upload

---
+++ Ejercicio1_Ev1_ELGM_EDP.py.py
@@ -0,0 +1,13 @@
+#Realiza un programa que debera preguntar al usuario:
+#Su edad y almacenarla en una variable. Despues comprueba si la edad es inferior a 18 años, si 
+#esta comparacion es cierta el programa debe decir que es menor de edad y si es falsa que es mayor.
+
+edad = int(input("Ingrese su edad \n>> "))
+
+if edad < 18:
+    print("Usted es menor de edad")
+else:
+    print("Usted es mayor de edad")
+
+
+
